46ca0636bb1bc9e800968b5aa5aab61aa208f91a
“gos0808” <gos08.08.23@gmail.com>
2024-05-08 22:11:36 -0400

    first commit

---
+++ src/Activities.js
@@ -0,0 +1,9 @@
+function Activities({ activity }) {
+    return (
+        <div>
+            <h2>{activity}</h2>
+        </div>
+    );
+}
+
+export default Activities;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import Activities from './Activities';
 
 function App() {
+
+  const [advice, setAdvice] = useState('');
+
+  const getAdviceData = async () => {
+    const response = await fetch('http://www.boredapi.com/api/activity/');
+    const data = await response.json();
+    setAdvice(data);
+  };
+
+  useEffect(() => {
+    getAdviceData();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='container'>
+        <h1>Looking for a new activity? Here are some ideas for you</h1>
+      </div>
+
+      <div className=' backgroundImage '>
+        <div className='activity container'>
+          <Activities activity={advice.activity} />
+          <button onClick={getAdviceData}>Find another activity</button>
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
